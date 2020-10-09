2c2384bad194cb9e9152d383783e5ad89a4f4c90
valjapan <yukinabe3561@icloud.com>
2020-10-09 15:01:29 +0900

    fix 1009

--- 001/get_cps_plot.m
+++ 001/get_cps_plot.m
@@ -1,8 +1,9 @@
 prompt = 'ファイル名を入力してください : ';
 prompt2 = '指定したい点を入力してください : ';
+prompt3 = 'FFTのサンプリング点数を入力してください : ';
 file = input(prompt);
 [y,fs]=audioread(file');
-N=512;
+N = input(prompt3);
 T = input(prompt2);
 xi=y(T:T+N-1).*hann(N);
 Xi=fft(xi);
@@ -10,7 +11,7 @@ spc=10*log10(abs(Xi).^2/N);
 f=(0:N-1)/N*fs;
 cps=ifft(spc); %ケプストラム
 lifter=ones(N,1);
-lifter(21:N-21+2)=0; % 21は適当(なんでや)
+lifter(21:N-21+2)=0; % 21は慣らし具合(なんでや)
 cps2=cps.*lifter;
 spc2=real(fft(cps2));
 plot(f,spc,f,spc2,'-r');
